aeb310eae2db83a09f54821c209448872e097bdf
secantalpha <zhangzheng7890@gmail.com>
2024-06-08 17:10:36 +0800

    fix compile on windows

--- setup.py
+++ setup.py
@@ -21,71 +21,37 @@ def find_eigen(min_ver=(3, 3, 0)):
     MAJOR_VER_STR = "#define EIGEN_MAJOR_VERSION"
     MINOR_VER_STR = "#define EIGEN_MINOR_VERSION"
     EIGEN_WEB_URL = (
-        "https://gitlab.com/libeigen/eigen/-/archive/3.3.7/eigen-3.3.7.tar.bz2"
+        "https://gitlab.com/libeigen/eigen/-/archive/3.4.0/eigen-3.4.0.tar.bz2"
     )
     TMP_EIGEN_FILE = "tmp_eigen.tar.bz2"
-    TMP_EIGEN_DIR = "eigen-3.3.7"
+    TMP_EIGEN_DIR = "eigen-3.4.0"
     min_ver_str = ".".join(map(str, min_ver))
 
-    eigen_path = None
-    for path in try_paths:
-        macros_path = os.path.join(path, "Eigen/src/Core/util/Macros.h")
-        if os.path.exists(macros_path):
-            macros = open(macros_path, "r").read().split("\n")
-            world_ver, major_ver, minor_ver = None, None, None
-            for line in macros:
-                if line.startswith(WORLD_VER_STR):
-                    world_ver = int(line[len(WORLD_VER_STR) :])
-                elif line.startswith(MAJOR_VER_STR):
-                    major_ver = int(line[len(MAJOR_VER_STR) :])
-                elif line.startswith(MINOR_VER_STR):
-                    minor_ver = int(line[len(MAJOR_VER_STR) :])
-            if not world_ver or not major_ver or not minor_ver:
-                print("Failed to parse macros file", macros_path)
-            else:
-                ver = (world_ver, major_ver, minor_ver)
-                ver_str = ".".join(map(str, ver))
-                if ver < min_ver:
-                    print(
-                        "Found unsuitable Eigen version",
-                        ver_str,
-                        "at",
-                        path,
-                        "(need >= " + min_ver_str + ")",
-                    )
-                else:
-                    print("Found Eigen version", ver_str, "at", path)
-                    eigen_path = path
-                    break
+    try:
+        import urllib.request
 
-    if eigen_path is None:
-        try:
-            import urllib.request
+        print("Couldn't find Eigen locally, downloading...")
+        req = urllib.request.Request(
+            EIGEN_WEB_URL,
+            data=None,
+            headers={
+                "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.135 Safari/537.36"
+            },
+        )
 
-            print("Couldn't find Eigen locally, downloading...")
-            req = urllib.request.Request(
-                EIGEN_WEB_URL,
-                data=None,
-                headers={
-                    "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.135 Safari/537.36"
-                },
-            )
+        with urllib.request.urlopen(req) as resp, open(TMP_EIGEN_FILE, "wb") as file:
+            data = resp.read()
+            file.write(data)
+        import tarfile
 
-            with urllib.request.urlopen(req) as resp, open(
-                TMP_EIGEN_FILE, "wb"
-            ) as file:
-                data = resp.read()
-                file.write(data)
-            import tarfile
+        tar = tarfile.open(TMP_EIGEN_FILE)
+        tar.extractall()
+        tar.close()
 
-            tar = tarfile.open(TMP_EIGEN_FILE)
-            tar.extractall()
-            tar.close()
-
-            eigen_path = TMP_EIGEN_DIR
-            os.remove(TMP_EIGEN_FILE)
-        except:
-            print("Download failed, failed to find Eigen")
+        eigen_path = TMP_EIGEN_DIR
+        os.remove(TMP_EIGEN_FILE)
+    except:
+        print("Download failed, failed to find Eigen")
 
     if eigen_path is not None:
         print("Found eigen at", eigen_path)
@@ -95,7 +61,7 @@ def find_eigen(min_ver=(3, 3, 0)):
 
 nvcc_flags = [
     "-O3",
-    "-std=c++14",
+    "-std=c++17",
     "--expt-extended-lambda",
     "--expt-relaxed-constexpr",
     "-U__CUDA_NO_HALF_OPERATORS__",
@@ -160,7 +126,7 @@ setup(
             ],
             include_dirs=[
                 os.path.join(_src_path, "include"),
-                find_eigen(),
+                os.path.join(_src_path, find_eigen()),
             ],
             extra_compile_args={
                 "cxx": c_flags,
