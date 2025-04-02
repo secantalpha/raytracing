201157a75db0034b2deee96f9fb59d99df9919f2
secantalpha <zhangzheng7890@gmail.com>
2025-04-02 23:15:46 +0800

    fake a CUDA::nvToolsExt target

--- cmake/Findnvtx.cmake
+++ cmake/Findnvtx.cmake
@@ -1,14 +1,4 @@
-FetchContent_Declare(
-  nvtx
-  GIT_REPOSITORY https://github.com/NVIDIA/NVTX.git
-  GIT_TAG tags/v3.1.1
-  GIT_SHALLOW TRUE)
-FetchContent_Populate(nvtx)
+include_guard()
 
-set(USE_SYSTEM_NVTX ON)
-list(APPEND CMAKE_PREFIX_PATH ${nvtx_SOURCE_DIR}/c/include)
 add_library(nvtx INTERFACE IMPORTED GLOBAL)
-
-target_include_directories(nvtx INTERFACE ${nvtx_SOURCE_DIR}/c/include)
-
 add_library(CUDA::nvToolsExt ALIAS nvtx)
