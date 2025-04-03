37adc294d3789b30ab5210711ca5569122fd1648
secantalpha <zhangzheng7890@gmail.com>
2025-04-04 00:31:18 +0800

    format

--- raytracing/raytracer.py
+++ raytracing/raytracer.py
@@ -1,20 +1,22 @@
-
 import numpy as np
 import torch
 
 # CUDA extension
 import _raytracing as _backend
 
-class RayTracer():
+
+class RayTracer:
     def __init__(self, vertices, triangles):
         # vertices: np.ndarray, [N, 3]
         # triangles: np.ndarray, [M, 3]
 
-        if torch.is_tensor(vertices): vertices = vertices.detach().cpu().numpy()
-        if torch.is_tensor(triangles): triangles = triangles.detach().cpu().numpy()
+        if torch.is_tensor(vertices):
+            vertices = vertices.detach().cpu().numpy()
+        if torch.is_tensor(triangles):
+            triangles = triangles.detach().cpu().numpy()
 
         assert triangles.shape[0] > 8, "BVH needs at least 8 triangles."
-        
+
         # implementation
         self.impl = _backend.create_raytracer(vertices, triangles)
 
@@ -26,8 +28,10 @@ class RayTracer():
         rays_o = rays_o.float().contiguous()
         rays_d = rays_d.float().contiguous()
 
-        if not rays_o.is_cuda: rays_o = rays_o.cuda()
-        if not rays_d.is_cuda: rays_d = rays_d.cuda()
+        if not rays_o.is_cuda:
+            rays_o = rays_o.cuda()
+        if not rays_d.is_cuda:
+            rays_d = rays_d.cuda()
 
         prefix = rays_o.shape[:-1]
         rays_o = rays_o.view(-1, 3)
@@ -44,9 +48,9 @@ class RayTracer():
             face_normals = rays_d
 
         depth = torch.empty_like(rays_o[:, 0])
-        
+
         # inplace write intersections back to rays_o
-        self.impl.trace(rays_o, rays_d, positions, face_normals, depth) # [N, 3]
+        self.impl.trace(rays_o, rays_d, positions, face_normals, depth)  # [N, 3]
 
         positions = positions.view(*prefix, 3)
         face_normals = face_normals.view(*prefix, 3)
